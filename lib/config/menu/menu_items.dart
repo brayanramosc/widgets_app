dd06483f04bb8843fbf681153cc4c77ba1e09cd9
Brayan Ramos <82058263+brayanrc@users.noreply.github.com>
2024-09-11 22:01:52 -0500

    feat: create menu config file

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subtitle;
+  final String url;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subtitle,
+    required this.url,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+      title: 'Botones',
+      subtitle: 'Varios botones de flutter',
+      url: '/buttons',
+      icon: Icons.smart_button_outlined,
+  ),
+
+  MenuItem(
+    title: 'Tarjetas',
+    subtitle: 'Contenedor estilizado',
+    url: '/card',
+    icon: Icons.credit_card,
+  ),
+];
